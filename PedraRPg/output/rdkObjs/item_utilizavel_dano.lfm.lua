698e9d72f9bc51e7522b166fc9db44409703509a
jingatoj <jingatoj@gmail.com>
2026-02-11 22:32:35 -0300

    Add files via upload

---
+++ PedraRPg/output/rdkObjs/item_utilizavel_dano.lfm.lua
@@ -0,0 +1,307 @@
+require("firecast.lua");
+local __o_rrpgObjs = require("rrpgObjs.lua");
+require("rrpgGUI.lua");
+require("rrpgDialogs.lua");
+require("rrpgLFM.lua");
+require("ndb.lua");
+require("locale.lua");
+local __o_Utils = require("utils.lua");
+
+local function constructNew_frmItemUtilizavelDano()
+    local obj = GUI.fromHandle(_obj_newObject("form"));
+    local self = obj;
+    local sheet = nil;
+
+    rawset(obj, "_oldSetNodeObjectFunction", obj.setNodeObject);
+
+    function obj:setNodeObject(nodeObject)
+        sheet = nodeObject;
+        self.sheet = nodeObject;
+        self:_oldSetNodeObjectFunction(nodeObject);
+    end;
+
+    function obj:setNodeDatabase(nodeObject)
+        self:setNodeObject(nodeObject);
+    end;
+
+    _gui_assignInitialParentForForm(obj.handle);
+    obj:beginUpdate();
+    obj:setName("frmItemUtilizavelDano");
+    obj:setHeight(340);
+
+
+    
+
+
+
+      require("sheetCode.lua");
+      require("ndb.lua");
+    
+
+
+
+  
+
+
+    obj.rectangle1 = GUI.fromHandle(_obj_newObject("rectangle"));
+    obj.rectangle1:setParent(obj);
+    obj.rectangle1:setAlign("client");
+    obj.rectangle1:setStrokeColor("#404040");
+    obj.rectangle1:setStrokeSize(1);
+    obj.rectangle1:setName("rectangle1");
+
+    obj.layout1 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout1:setParent(obj);
+    obj.layout1:setAlign("top");
+    obj.layout1:setHeight(6);
+    obj.layout1:setName("layout1");
+
+    obj.layout2 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout2:setParent(obj);
+    obj.layout2:setAlign("top");
+    obj.layout2:setHeight(34);
+    obj.layout2:setName("layout2");
+
+    obj.label1 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label1:setParent(obj.layout2);
+    obj.label1:setText("Item:");
+    obj.label1:setAlign("left");
+    obj.label1:setWidth(40);
+    obj.label1:setName("label1");
+
+    obj.edit1 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit1:setParent(obj.layout2);
+    obj.edit1:setField("nome");
+    obj.edit1:setAlign("left");
+    obj.edit1:setWidth(260);
+    obj.edit1:setName("edit1");
+
+    obj.label2 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label2:setParent(obj.layout2);
+    obj.label2:setText("Usos:");
+    obj.label2:setAlign("left");
+    obj.label2:setWidth(40);
+    obj.label2:setName("label2");
+
+    obj.edit2 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit2:setParent(obj.layout2);
+    obj.edit2:setField("usosAtual");
+    obj.edit2:setAlign("left");
+    obj.edit2:setWidth(45);
+    obj.edit2:setType("number");
+    obj.edit2:setName("edit2");
+
+    obj.label3 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label3:setParent(obj.layout2);
+    obj.label3:setText("/");
+    obj.label3:setAlign("left");
+    obj.label3:setWidth(10);
+    obj.label3:setName("label3");
+
+    obj.edit3 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit3:setParent(obj.layout2);
+    obj.edit3:setField("usosMax");
+    obj.edit3:setAlign("left");
+    obj.edit3:setWidth(45);
+    obj.edit3:setType("number");
+    obj.edit3:setName("edit3");
+
+    obj.button1 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button1:setParent(obj.layout2);
+    obj.button1:setText("Usar");
+    obj.button1:setAlign("left");
+    obj.button1:setWidth(70);
+    obj.button1:setName("button1");
+
+    obj.layout3 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout3:setParent(obj.layout2);
+    obj.layout3:setAlign("client");
+    obj.layout3:setName("layout3");
+
+    obj.button2 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button2:setParent(obj.layout2);
+    obj.button2:setText("Remover");
+    obj.button2:setAlign("right");
+    obj.button2:setWidth(90);
+    obj.button2:setName("button2");
+
+    obj.layout4 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout4:setParent(obj);
+    obj.layout4:setAlign("top");
+    obj.layout4:setHeight(8);
+    obj.layout4:setName("layout4");
+
+    obj.layout5 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout5:setParent(obj);
+    obj.layout5:setAlign("top");
+    obj.layout5:setHeight(34);
+    obj.layout5:setName("layout5");
+
+    obj.label4 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label4:setParent(obj.layout5);
+    obj.label4:setText("Dano:");
+    obj.label4:setAlign("left");
+    obj.label4:setWidth(50);
+    obj.label4:setName("label4");
+
+    obj.edit4 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit4:setParent(obj.layout5);
+    obj.edit4:setField("dano");
+    obj.edit4:setAlign("left");
+    obj.edit4:setWidth(140);
+    obj.edit4:setName("edit4");
+
+    obj.label5 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label5:setParent(obj.layout5);
+    obj.label5:setText("Extra:");
+    obj.label5:setAlign("left");
+    obj.label5:setWidth(50);
+    obj.label5:setName("label5");
+
+    obj.edit5 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit5:setParent(obj.layout5);
+    obj.edit5:setField("danoExtra");
+    obj.edit5:setAlign("left");
+    obj.edit5:setWidth(160);
+    obj.edit5:setName("edit5");
+
+    obj.button3 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button3:setParent(obj.layout5);
+    obj.button3:setText("Rolar");
+    obj.button3:setAlign("left");
+    obj.button3:setWidth(80);
+    obj.button3:setName("button3");
+
+    obj.layout6 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout6:setParent(obj.layout5);
+    obj.layout6:setAlign("client");
+    obj.layout6:setName("layout6");
+
+    obj.layout7 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout7:setParent(obj);
+    obj.layout7:setAlign("top");
+    obj.layout7:setHeight(8);
+    obj.layout7:setName("layout7");
+
+    obj.layout8 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout8:setParent(obj);
+    obj.layout8:setAlign("top");
+    obj.layout8:setHeight(240);
+    obj.layout8:setName("layout8");
+
+    obj.image1 = GUI.fromHandle(_obj_newObject("image"));
+    obj.image1:setParent(obj.layout8);
+    obj.image1:setField("imgItem");
+    obj.image1:setEditable(true);
+    obj.image1:setStyle("autoFit");
+    obj.image1:setAlign("left");
+    obj.image1:setWidth(180);
+    obj.image1:setHeight(180);
+    obj.image1:setName("image1");
+
+    obj.layout9 = GUI.fromHandle(_obj_newObject("layout"));
+    obj.layout9:setParent(obj.layout8);
+    obj.layout9:setAlign("left");
+    obj.layout9:setWidth(10);
+    obj.layout9:setName("layout9");
+
+    obj.textEditor1 = GUI.fromHandle(_obj_newObject("textEditor"));
+    obj.textEditor1:setParent(obj.layout8);
+    obj.textEditor1:setField("descricao");
+    obj.textEditor1:setAlign("client");
+    obj.textEditor1:setName("textEditor1");
+
+    obj._e_event0 = obj.button1:addEventListener("onClick",
+        function (event)
+            usarConsumivel(sheet);
+        end);
+
+    obj._e_event1 = obj.button2:addEventListener("onClick",
+        function (event)
+            local r=getRootSheet(sheet); ndb.deleteNode(sheet); if r~=nil then recalc(r); end
+        end);
+
+    obj._e_event2 = obj.button3:addEventListener("onClick",
+        function (event)
+            rolarDanoUtilizavel(sheet);
+        end);
+
+    function obj:_releaseEvents()
+        __o_rrpgObjs.removeEventListenerById(self._e_event2);
+        __o_rrpgObjs.removeEventListenerById(self._e_event1);
+        __o_rrpgObjs.removeEventListenerById(self._e_event0);
+    end;
+
+    obj._oldLFMDestroy = obj.destroy;
+
+    function obj:destroy() 
+        self:_releaseEvents();
+
+        if (self.handle ~= 0) and (self.setNodeDatabase ~= nil) then
+          self:setNodeDatabase(nil);
+        end;
+
+        if self.layout8 ~= nil then self.layout8:destroy(); self.layout8 = nil; end;
+        if self.label2 ~= nil then self.label2:destroy(); self.label2 = nil; end;
+        if self.button2 ~= nil then self.button2:destroy(); self.button2 = nil; end;
+        if self.rectangle1 ~= nil then self.rectangle1:destroy(); self.rectangle1 = nil; end;
+        if self.edit4 ~= nil then self.edit4:destroy(); self.edit4 = nil; end;
+        if self.edit3 ~= nil then self.edit3:destroy(); self.edit3 = nil; end;
+        if self.layout3 ~= nil then self.layout3:destroy(); self.layout3 = nil; end;
+        if self.label5 ~= nil then self.label5:destroy(); self.label5 = nil; end;
+        if self.textEditor1 ~= nil then self.textEditor1:destroy(); self.textEditor1 = nil; end;
+        if self.layout6 ~= nil then self.layout6:destroy(); self.layout6 = nil; end;
+        if self.layout9 ~= nil then self.layout9:destroy(); self.layout9 = nil; end;
+        if self.label3 ~= nil then self.label3:destroy(); self.label3 = nil; end;
+        if self.button3 ~= nil then self.button3:destroy(); self.button3 = nil; end;
+        if self.image1 ~= nil then self.image1:destroy(); self.image1 = nil; end;
+        if self.edit5 ~= nil then self.edit5:destroy(); self.edit5 = nil; end;
+        if self.layout1 ~= nil then self.layout1:destroy(); self.layout1 = nil; end;
+        if self.layout4 ~= nil then self.layout4:destroy(); self.layout4 = nil; end;
+        if self.layout7 ~= nil then self.layout7:destroy(); self.layout7 = nil; end;
+        if self.label1 ~= nil then self.label1:destroy(); self.label1 = nil; end;
+        if self.button1 ~= nil then self.button1:destroy(); self.button1 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.layout2 ~= nil then self.layout2:destroy(); self.layout2 = nil; end;
+        if self.label4 ~= nil then self.label4:destroy(); self.label4 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.layout5 ~= nil then self.layout5:destroy(); self.layout5 = nil; end;
+        self:_oldLFMDestroy();
+    end;
+
+    obj:endUpdate();
+
+    return obj;
+end;
+
+function newfrmItemUtilizavelDano()
+    local retObj = nil;
+    __o_rrpgObjs.beginObjectsLoading();
+
+    __o_Utils.tryFinally(
+      function()
+        retObj = constructNew_frmItemUtilizavelDano();
+      end,
+      function()
+        __o_rrpgObjs.endObjectsLoading();
+      end);
+
+    assert(retObj ~= nil);
+    return retObj;
+end;
+
+local _frmItemUtilizavelDano = {
+    newEditor = newfrmItemUtilizavelDano, 
+    new = newfrmItemUtilizavelDano, 
+    name = "frmItemUtilizavelDano", 
+    dataType = "", 
+    formType = "undefined", 
+    formComponentName = "form", 
+    cacheMode = "none", 
+    title = "", 
+    description=""};
+
+frmItemUtilizavelDano = _frmItemUtilizavelDano;
+Firecast.registrarForm(_frmItemUtilizavelDano);
+
+return _frmItemUtilizavelDano;
